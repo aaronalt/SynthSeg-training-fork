203d54743bfac8c66c98b8fd5377d9561d34329c
aaronalt <aaronalt07@gmail.com>
2026-02-03 18:25:45 +0100

    3t_val edits

--- scripts/custom/training.py
+++ scripts/custom/training.py
@@ -72,15 +72,19 @@ all_val_paths = str(val_path)
 standardize_training_labels(train_path, train_path)
 standardize_training_labels(val_path, val_path)
 
-# Filter validation to only 3T data
+# Filter validation to only 3T data - create symlink directory
 from ext.lab2im import utils as lab2im_utils
+import shutil
 val_files = lab2im_utils.list_images_in_folder(val_path)
 val_files_3T = [f for f in val_files if '3T' in os.path.basename(f)]
-val_3T_list = '/home/althause/data/training_split/val_3T_files.txt'
-with open(val_3T_list, 'w') as f:
-    f.write('\n'.join(val_files_3T))
-print(f"Filtered validation to {len(val_files_3T)}/{len(val_files)} 3T files")
-all_val_paths = val_3T_list
+val_3T_dir = '/home/althause/data/training_split/val_3T'
+if os.path.exists(val_3T_dir):
+    shutil.rmtree(val_3T_dir)
+os.makedirs(val_3T_dir)
+for f in val_files_3T:
+    os.symlink(f, os.path.join(val_3T_dir, os.path.basename(f)))
+print(f"Filtered validation to {len(val_files_3T)}/{len(val_files)} 3T files in {val_3T_dir}")
+all_val_paths = val_3T_dir
 val_subjects_prob = None
 # Pre-trained model
 path_checkpoint = '/home/althause/data/weights/mauri_unet_weights.h5'

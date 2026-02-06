c4a1a142543376e7479d9534c0668e18c5364dc6
aaronalt <aaronalt07@gmail.com>
2026-02-06 21:18:10 +0100

    added gt and prediction.py

--- scripts/custom/prediction.py
+++ scripts/custom/prediction.py
@@ -45,8 +45,8 @@ np.save('./data/labels_classes_priors/topology_classes.npy', topology_classes)
 # Find all model checkpoints and sort by epoch number
 model_dir = '/home/althause/SynthSeg-training-fork/models/test/experiment_20260205_095904'
 model_files = sorted(glob(os.path.join(model_dir, '*.h5')))
-model_files = model_files[:10]
-# model_files = [f for f in model_files if 'dice_finetune_064' in f]
+# model_files = model_files[:10]
+model_files = [f for f in model_files if 'dice_finetune_031' in f]
 # Ground truth directories for evaluation
 gt_dirs = {
     '7T': [

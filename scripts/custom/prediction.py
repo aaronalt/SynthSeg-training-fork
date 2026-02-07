7862c840c0211df2662525e02fcc02f0c6eabd83
aaronalt <aaronalt07@gmail.com>
2026-02-07 19:28:48 +0100

    prediction

--- scripts/custom/prediction.py
+++ scripts/custom/prediction.py
@@ -21,7 +21,7 @@ from pathlib import Path
 import compare_dice_batch as evaluate
 
 # === OPTIONS ===
-DELETE_TMP_PREDICTIONS = True  # Set to True to delete segmentations after evaluation (keeps CSVs)
+DELETE_TMP_PREDICTIONS = False  # Set to True to delete segmentations after evaluation (keeps CSVs)
 FIELD_STRENGTH = '3T'
 
 # Store results across all models for summary
@@ -43,7 +43,7 @@ np.save('./data/labels_classes_priors/segmentation_labels.npy', segmentation_lab
 np.save('./data/labels_classes_priors/topology_classes.npy', topology_classes)
 
 # Find all model checkpoints and sort by epoch number
-model_dir = '/home/althause/SynthSeg-training-fork/models/test/experiment_20260204_215023'
+model_dir = '/home/althause/SynthSeg-training-fork/models/test/experiment_20260207_110143'
 model_files = sorted(glob(os.path.join(model_dir, '*.h5')))
 # model_files = model_files[31:60]
 # model_files = [f for f in model_files if 'dice_finetune_031' in f]

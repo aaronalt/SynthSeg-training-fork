ed6deaefd2e9db07294365e2afee6ede0736cd4f
aaronalt <aaronalt07@gmail.com>
2026-02-07 08:51:19 +0100

    fixing loss class errors

--- scripts/custom/training.py
+++ scripts/custom/training.py
@@ -116,7 +116,7 @@ feat_multiplier = 2
 
 # Training parameters
 lr = 1e-4
-wl2_epochs = 2  # Skip warmup - using pretrained weights
+wl2_epochs = 0  # Skip warmup - using pretrained weights
 dice_epochs = 100  # Train longer to learn small structures
 steps_per_epoch = 5000
 validation_steps = 200  # More steps for stable validation with small 3T set

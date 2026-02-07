ba6824cded0c6dc99535cbc697116d64fe38d05f
aaronalt <aaronalt07@gmail.com>
2026-02-07 19:49:59 +0100

    prediction

--- scripts/custom/prediction.py
+++ scripts/custom/prediction.py
@@ -22,7 +22,7 @@ import compare_dice_batch as evaluate
 
 # === OPTIONS ===
 DELETE_TMP_PREDICTIONS = False  # Set to True to delete segmentations after evaluation (keeps CSVs)
-FIELD_STRENGTH = '3T'
+FIELD_STRENGTH = '7T'
 
 # Store results across all models for summary
 all_model_results = []

1063caedd5f2f775de8ed08670191765c08952f4
aaronalt <aaronalt07@gmail.com>
2026-02-23 14:58:24 +0100

    prediction

--- scripts/custom/prediction.py
+++ scripts/custom/prediction.py
@@ -101,7 +101,7 @@ for path_model in model_files:
     exp = model_dir.split('/')[-1]
     model_name = os.path.basename(path_model).replace('.h5', '')
     model = os.path.basename(path_model)
-    path_images = '/home/althause/data/TEST/3t7t'
+    path_images = '/home/althause/data/TEST'
 
     # Determine field strength (manual override or auto-detect)
     if FORCE_FIELD_STRENGTH:
